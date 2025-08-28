bd98784bd10092080283fb488bf60850c4289465
patricia-gois-87 <meetupcloud@outlook.com>
2025-08-28 01:34:47 -0300

    code terraform

--- ec2.tf
+++ ec2.tf
@@ -14,11 +14,20 @@ data "aws_ami" "amazon_linux" {
 }
 
 
+
 resource "aws_instance" "vm" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.main_subnet.id
   vpc_security_group_ids = [aws_security_group.vm_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo 'ec2-user:Su1zan1e@' | chpasswd
+              sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+              sed -i 's/^PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
+              systemctl restart sshd
+              EOF
 
   tags = {
     Name = var.instance_name
